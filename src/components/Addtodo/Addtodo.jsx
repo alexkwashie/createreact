ddb8c347ac6f769cc81f47ba791514906d6ba65d
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-08-30 23:40:21 +0100

    Adding text input form input

--- src/components/Addtodo/Addtodo.jsx
+++ src/components/Addtodo/Addtodo.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 
-function Addtodo() {
+class Addtodo extends React.Component {
+    state ={
+        task:''
+    }
+
+    onChangemethod = (e) => {
+        this.setState({task: e.target.value})
+    }
+
+render() {
     return (
         <form style={{display: 'flex',}}>
-        <input type="text" name="task" placeholder="Add Todo ..." style={{flex: '9', padding: '5px'}}/>
+        <input type="text" name="task" placeholder="Add Todo ..." style={{flex: '9', padding: '5px'}} value={this.state.task} onChange = {this.onChangemethod} />
 
-        <input type="submit" value="submit" style={{flex: '1'}} className= 'btn'/>
+        <input type="submit" value="submit" style={{flex: '1', background: '#77c722'}} className= 'btn'/>
         </form>
 
     )
 }
 
+}
 export default Addtodo
