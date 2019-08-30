5e86cc873e9d0e6b39aa9bc0b71024871ced5848
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-08-30 20:19:53 +0100

    adding CSS

--- src/components/Todolist/Todolist.js
+++ src/components/Todolist/Todolist.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class Todolist extends Component {
-    getStyle = ()=>{
+    getStyle = () =>{
         return {
+            background: '#f4f4f4',
+            padding:'10px',
+            borderBottom: 'dotted black 1px',
             textDecoration: this.props.todo.completed ? 'line-through' : 'none'}
-        }
     }
 
-    
-    render() {
+    render(){
         return (
             <div style = {this.getStyle()}>
-                <h3>{this.props.todo.task}</h3>
+                <h3>
+                {}
+                {this.props.todo.task}
+                </h3>
             </div>
         )
     }
 }
+
+
+
+//PropType are a validation for what props should have eg: required etc
+Todolist.propTypes = {
+    todo:PropTypes.object.isRequired
+    }
 
 export default Todolist
--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Todolist from '../Todolist/Todolist';
-import
+import PropTypes from 'prop-types';
+
 
 class Todos extends React.Component {
-  render(){
-
+  render() {
     return this.props.todo.map((tdo)=>(
         <Todolist key={tdo.id} todo = {tdo}/> //todo is the prop
     ));
-
   }
 
+}
+
+//PropType are a validation for what props should have eg: required etc
+Todos.propTypes = {
+todo:PropTypes.array.isRequired
 }
 
 export default Todos;
